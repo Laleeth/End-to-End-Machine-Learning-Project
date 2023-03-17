993a1ab9c61b9bf45fc109c45d541121a934d87d
lalith <lalith.hb@gmail.com>
2023-03-17 21:45:40 +0100

    Data Transformation file update
    Data Transformation file update

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,7 +3,7 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
-
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -21,10 +21,10 @@ class DataIngestion:
     def initate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df =pd.read_csv('notebook/data/data.csv')
+            df =pd.read_csv('notebook\data\stud.csv')
             logging.info("Read the dataset as DataFrame")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path).exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info("Train test split")
@@ -41,3 +41,10 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
+        
+if __name__ =="__main__":
+    obj = DataIngestion()
+    train_data,test_data = obj.initate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
